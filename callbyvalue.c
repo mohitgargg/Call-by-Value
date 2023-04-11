4b51c2017c7b25945aeef118827fea17f2e0f8cb
mohitgargg <125942814+mohitgargg@users.noreply.github.com>
2023-04-11 13:14:52 +0530

    Add files via upload

---
+++ callbyvalue.c
@@ -0,0 +1,17 @@
+#include <windows.h>
+#include<stdio.h>
+
+void disp(int);
+	
+	int main()
+	{
+		int x=2;
+		disp(x);
+		printf("%d",x);
+	}
+	void disp(int a)
+	{
+		printf("%d",a);
+		a=a+1;
+	}
+
